e02d529ed0b3dd28ed54d8c2e5bed3b8b4496fa3
momin-mubash <mubashmomin7@gmail.com>
2026-01-19 20:13:30 +0530

    concrete

--- concreteView/api/views.py
+++ concreteView/api/views.py
@@ -2,11 +2,11 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
 
-class StudentListCreate(ListAPIView,CreateAPIView):#ListCreateAPIView can also be used
+class StudentListCreate(ListAPIView,CreateAPIView):#ListCreateAPIView can also be used here
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-class StudentRetrieveUpdateDelete(RetrieveAPIView,UpdateAPIView,DestroyAPIView):#RetrieveUpdateDestroyAPIView can also be used
+class StudentRetrieveUpdateDelete(RetrieveAPIView,UpdateAPIView,DestroyAPIView):#RetrieveUpdateDestroyAPIView can also be used here
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
