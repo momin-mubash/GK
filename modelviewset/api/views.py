cc5bb31ef19074b9d669d5324c762542eb8cd0a7
momin-mubash <mubashmomin7@gmail.com>
2026-01-25 07:44:05 +0530

    modelviewset

--- modelviewset/api/views.py
+++ modelviewset/api/views.py
@@ -7,6 +7,7 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+
 #there is also one readonlymodelviewset concept similar to this modelviewset
 #which provides read-only operations (list and retrieve) for the model.
 #all the things are same as above replace modelviewset with readonlymodelviewset
