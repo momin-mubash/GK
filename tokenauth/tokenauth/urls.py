125c38df4162f3af34a26785ab663a421c118c18
momin-mubash <mubashmomin7@gmail.com>
2026-01-30 07:46:33 +0530

    tokenauth

---
+++ tokenauth/tokenauth/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path , include
+from api import views
+from rest_framework.routers import DefaultRouter
+# from rest_framework.authtoken.views import obtain_auth_token
+from api.auth import CustomAuthToken
+
+
+#cretaing router object
+router = DefaultRouter()
+#register StudentModelViewSet with router
+router.register('studentapi',views.StudentModelViewSet,basename='student')
+
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('',include(router.urls)),
+    # path('getoken/',obtain_auth_token),#user hit the url gettoken/ with username and password to get token(httpie)
+    path('gettoken/',CustomAuthToken.as_view()),#to get custom response here token,id and email
+
+]
