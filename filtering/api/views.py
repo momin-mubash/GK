6d675f0f09d357a91a092abe7bd8210938a26055
momin-mubash <mubashmomin7@gmail.com>
2026-03-04 02:16:02 +0530

    filtering

---
+++ filtering/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.generics import ListAPIView
+
+# Create your views here.
+class StudentList(ListAPIView):
+    # queryset = Student.objects.filter(passby='user1')
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer    
+    def get_queryset(self):
+        user = self.request.user
+        return Student.objects.filter(passby=user)#shows only user's own data
+    
+#prefer notes for two more filtering concepts:queryparameters ,DjangoFilterBackend
+
